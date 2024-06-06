52008ba30bbcdb7a6fc5c1ce402bfea829e33099
Amol Khedekar <amolkhedekar1632@gmail.com>
2024-06-06 19:40:10 +0530

    BloodFynd - Blood Bank System Done

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/BloodBankManagement'),
+      dir: require('path').join(__dirname, './coverage/bloodfynd'),
       subdir: '.',
       reporters: [
         { type: 'html' },
